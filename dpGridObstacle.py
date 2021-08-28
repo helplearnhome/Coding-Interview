940745fc05e50fdd21ffc1df449614e86e15490e
Help Learn Home <helplearnhome@protonmail.com>
2021-08-28 11:23:56 +0530

    dbGridObstacle

--- dpGridObstacle.py
+++ dpGridObstacle.py
@@ -1,17 +1,29 @@
-
-def gridPaths(n,m):
-   global grid
-   if n == len(grid) or m == len(grid[0]):
-       return 0
-   if grid[n][m] == '*':
-       return 0
-   if n == len(grid)-1 and m == len(grid[0])-1:
-       return 1
-   return gridPaths(n,m+1)+gridPaths(n+1,m)
+def fact(x):
+    if x == 0:
+        return 1
+    return x*fact(x-1)
+def gridPaths(n):
+   return fact((n-1)*2)/(fact(n-1)*fact(n-1))
 
 
-n=int(input())
-grid=[]
-for _ in range(n):
-    grid.append(list(input()))
-print(gridPaths(0,0))
+# n=int(input())
+# grid=[]
+# for _ in range(n):
+#     grid.append(list(input()))
+n=3
+grid=[
+    ['.','.','*'],
+    ['.','*','.'],
+    ['.','.','*']
+]
+set1=set()
+count=0
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == "*":
+            set1.add((i,j))
+print(set1)
+print(len(set1))    
+print(set1)           
+print(int(gridPaths(n)))
